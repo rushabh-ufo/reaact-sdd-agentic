fa87dcc3f7455d55a7d0c4510c75b040fdee3a7f
rushabh-ufo <rushabh_ufo@yahoo.com>
2026-06-15 14:23:04 +0530

    first commit

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  collectCoverageFrom: [
+    "pages/**/*.{ts,tsx}",
+    "components/**/*.{ts,tsx}",
+    "!**/*.d.ts",
+  ],
+};
+
+export default createJestConfig(config);
